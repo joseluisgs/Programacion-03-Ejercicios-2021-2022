4d8858d6e6ed61bed10ab457475076a7d39007a5
Bank <elroby444@gmail.com>
2021-11-25 10:16:11 +0100

    Ejercicio 2 examen

---
+++ soluciones/Examen-01-02-RBlazquez/src/com/company/Utils/Constantes.java
@@ -0,0 +1,20 @@
+package com.company.Utils;
+
+public final class Constantes {
+    /**
+     * Resultado jugador gana
+     */
+    public static final int JUGADOR = 1;
+    /**
+     * Resultado computadora gana
+     */
+    public static final int COMPUTADORA = 2;
+    /**
+     * Resultado empate, ninguno gana
+     */
+    public static final int EMPATE = 0;
+    /**
+     * Canicas con las que se empieza
+     */
+    public static final int MAXCANICAS = 10;
+}
